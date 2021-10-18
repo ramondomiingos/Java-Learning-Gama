945dc6f4e2ba53956a146b9e6b02a8e43ed30588
Ramon Oliveira <ramon.oliveira@activesoftconsultoria.com.br>
2021-10-18 13:12:24 -0300

    Adiçaõ de testes

---
+++ src/com/ramon/study/Week2/test/FuncionarioTest.java
@@ -0,0 +1,16 @@
+package com.ramon.study.Week2.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionarioTest {
+
+    @Test
+    void getDepartamento() {
+    }
+
+    @Test
+    void setDepartamento() {
+    }
+}
---
+++ src/com/ramon/study/Week2/test/VipTest.java
@@ -0,0 +1,16 @@
+package com.ramon.study.Week2.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VipTest {
+
+    @Test
+    void imprimeValor() {
+    }
+
+    @Test
+    void mostrarTaxas() {
+    }
+}
